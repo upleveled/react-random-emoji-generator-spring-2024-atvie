f1e3e2641ee42a7714e44e9b16c22c8a8ed23711
Lukas Prochazka <prochazka.lu@gmail.com>
2024-05-14 16:49:26 +0200

    Update Styling and variable names

--- src/App.js
+++ src/App.js
@@ -1,57 +1,77 @@
-// completely random emoji
-// choosing an emoji by name
-// showing error if invalid emoji name entered
 import { get, has, random } from 'node-emoji';
 import { useState } from 'react';
 
 export default function App() {
   // { name: 'house', emoji: '🏠' }
   const initialEmoji = random();
-  // 1. create a state for the input
-  const [emojiName, setEmojiName] = useState(initialEmoji.name);
-  const [emojiIcon, setEmojiIcon] = useState(initialEmoji.emoji);
+  // 1. Create a state for the input
+  const [emoji, setEmoji] = useState(initialEmoji.emoji);
+  const [emojiInput, setEmojiInput] = useState(initialEmoji.name);
 
-  const hasEmoji = has(emojiName);
+  const hasEmoji = has(emojiInput);
 
   return (
-    <>
+    <div
+      style={{
+        fontFamily: 'sans-serif',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
       <h1>Random Emoji Generator</h1>
-      <input
-        // 2. Set the value to the input
-        value={emojiName}
-        onChange={(event) => {
-          // set Emoji mame to input value
-          setEmojiName(event.currentTarget.value);
-          // get Emoji from the input value
-          const foundEmojiIcon = get(event.currentTarget.value);
-          // set Emoji icon to found emoji
-          setEmojiIcon(foundEmojiIcon);
-        }}
-      />
-      <button
-        onClick={() => {
-          const newEmoji = random();
-          setEmojiIcon(newEmoji.emoji);
-          setEmojiName(newEmoji.name);
-        }}
-      >
-        Generate Random
-      </button>
-      <div style={{ fontSize: '100px' }}>{emojiIcon}</div>
-      {/* Boolean Operator */}
+      {hasEmoji ? (
+        <>
+          <div style={{ fontSize: '100px' }}>{emoji}</div>
+          <div
+            style={{
+              backgroundColor: '#F0F0F0',
+              padding: '5px 10px',
+              marginBottom: '16px',
+              borderRadius: '16px',
+            }}
+          >
+            {emojiInput}
+          </div>
+        </>
+      ) : (
+        <div style={{ fontSize: '100px', height: '176px' }} />
+      )}
+      <div>
+        <input
+          // 2. Set the value to the input
+          value={emojiInput}
+          onChange={(event) => {
+            // 3. Set Emoji name to input value
+            setEmojiInput(event.currentTarget.value);
+            // get Emoji from the input value
+            const foundEmoji = get(event.currentTarget.value);
+            // set Emoji icon to found emoji
+            setEmoji(foundEmoji);
+          }}
+        />
+        <button
+          onClick={() => {
+            const newEmoji = random();
+            setEmoji(newEmoji.emoji);
+            setEmojiInput(newEmoji.name);
+          }}
+        >
+          Generate Random
+        </button>
+      </div>
       {!hasEmoji && (
-        <div style={{ backgroundColor: 'red', marginTop: '5px' }}>
+        <div
+          style={{
+            backgroundColor: '#f7b7b7',
+            borderRadius: '16px',
+            padding: '5px 10px 5px 10px',
+            marginTop: '16px',
+          }}
+        >
           This emoji doesn't exist
         </div>
       )}
-      {/* Ternary Operator
-      {hasEmoji ? (
-        <div style={{ fontSize: '100px' }}>{emojiIcon}</div>
-      ) : (
-        <div style={{ backgroundColor: 'red', marginTop: '5px' }}>
-          This emoji doesn't exist
-        </div>
-      )} */}
-    </>
+    </div>
   );
 }
